951b8c846bb19fda1d011c9342a694c528521d55
thirtysix <assbreak3r@gmail.com>
2023-05-05 15:02:56 +0300

    Add webcam window rule.

--- config.h
+++ config.h
@@ -30,12 +30,14 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class        instance  title      tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",       NULL,     NULL,      0,         1,          0,           0,        -1 },
-	{ "Firefox",    NULL,     NULL,      1 << 8,    0,          0,          -1,        -1 },
-	{ "St",         NULL,     NULL,      0,         0,          1,           0,        -1 },
-	{ "scratchpad", NULL,     NULL,      0,         1,          1,           0         -1 },
-	{ NULL,         NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+	/* class        instance        title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "Gimp",       NULL,           NULL,           0,         1,          0,           0,        -1 },
+	{ "Firefox",    NULL,           NULL,           1 << 8,    0,          0,          -1,        -1 },
+	{ "St",         NULL,           NULL,           0,         0,          1,           0,        -1 },
+	{ "scratchpad", "scratchpad",   NULL,           0,         1,          1,           0,        -1 },
+	{ "Alacritty",  "Alacritty",    NULL,           0,         0,          1,           0,        -1 },
+	{ NULL,         NULL,          "webcam",        0,         1,          0,           0,        -1 },
+	{ NULL,         NULL,           "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
@@ -65,26 +67,26 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 
 /* #include "movestack.c" */
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	/* { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } }, */
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_g,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_u,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_space , zoom,           {0} },
+	{ MODKEY,                       XK_space,  zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
-	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_i,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_y,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_f,      togglefullscr,  {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
@@ -95,7 +97,7 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	{ MODKEY,                       XK_z,      setgaps,        {.i = -1 } },
 	{ MODKEY,                       XK_x,      setgaps,        {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_0,      setgaps,        {.i = 0  } },
+	{ MODKEY|ShiftMask,             XK_g,      setgaps,        {.i = 0  } },
     { MODKEY,                       XK_F5,     xrdb,           {.v = NULL } },
  	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
  	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
